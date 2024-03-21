ed53c2531307ec888ba937c6393cf641ad9b176f
AlexBalogh1933 <stickmanaloox@gmail.com>
2024-03-21 14:20:23 -0400

    Finished DnD Map Page

--- js/index.js
+++ js/index.js
@@ -1,16 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let slideIndex = 0;
-  showSlides();
+  let slideIndex1 = 0;
+  showSlides1();
 
-  function showSlides() {
+  function showSlides1() {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
+    let slides = document.getElementsByClassName("mySlides1");
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 8000); // Change image every 8 seconds
+    slideIndex1++;
+    if (slideIndex1 > slides.length) {slideIndex1 = 1}
+    slides[slideIndex1-1].style.display = "block";
+    setTimeout(showSlides1, 8000); // Change image every 8 seconds
+  }
+
+  let slideIndex2 = 0;
+  showSlides2();
+
+  function showSlides2() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides2");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex2++;
+    if (slideIndex2 > slides.length) {slideIndex2 = 1}
+    slides[slideIndex2-1].style.display = "block";
+    setTimeout(showSlides2, 5000); // Change image every 8 seconds
   }
 });
